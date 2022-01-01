364564f09be542b9c66d43540d27e271102dbea1
Renzo Manganiello <renzo@fmanganiello.com.ar>
2022-01-01 19:31:28 -0300

    Generate Github Pages index with Jinja and some adjustments
    - Add more properties to sites objects (currency, country, etc.)
    - Change report format make it usable with Jinja2
    - Add sites images
    - Customize css rules for Cayman templates

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import { chromium } from 'playwright'
 
 const sites = [
   {
-    vendor: 'Sony AR',
+    vendor: 'Sony Store',
+    slug: 'sony-store-ar',
+    logoPath: './assets/img/sony-logo.png',
+    country: 'AR',
+    currency: 'ARS',
     url: 'https://store.sony.com.ar/ps5?cpint=PS5-Landing-SEP-29-2020',
     usesPagination: true,
     getPageNumberQueryString: ({ pageNumber }) => `&page=${pageNumber}`,
@@ -20,7 +24,11 @@ const sites = [
         const title = await item.$eval('.vtex-product-summary-2-x-productBrand', element => element.innerText)
         if (title.includes('PS5 Estandar')) {
           const outOfStock = await item.$('.vtex-rich-text-0-x-paragraph--producto-sin-stock')
+          if (outOfStock) {
+            continue
+          }
           const url = await item.$eval('.vtex-product-summary-2-x-clearLink', element => element.href)
+          const thumbnail = await item.$eval('img', element => element.src)
           let price = null
           if (!outOfStock) {
             price = await item.$eval('.vtex-product-price-1-x-currencyContainer', element => element.innerText)
@@ -29,7 +37,7 @@ const sites = [
             title,
             url,
             price,
-            hasStock: !outOfStock
+            thumbnail
           })
         }
       }
@@ -37,8 +45,12 @@ const sites = [
     }
   },
   {
-    vendor: 'Fravega AR',
+    vendor: 'Fravega',
+    slug: 'fravega-ar',
     url: 'https://www.fravega.com/l/?keyword=playstation+5',
+    logoPath: './assets/img/fravega-logo.webp',
+    country: 'AR',
+    currency: 'ARS',
     usesPagination: true,
     getPageNumberQueryString: ({ pageNumber }) => `&page=${pageNumber}`,
     checkEmptyPage: async ({ page }) => {
@@ -54,11 +66,12 @@ const sites = [
         if (title.includes('Consola Sony PlayStation 5')) {
           const url = await item.$eval('a', element => element.href)
           const price = await item.$eval('div[data-test-id=product-price]', element => element.innerText)
+          const thumbnail = await item.$eval('img', element => element.src)
           ps5Items.push({
             title,
             url,
             price,
-            hasStock: true
+            thumbnail
           })
         }
       }
@@ -67,7 +80,11 @@ const sites = [
   },
   {
     vendor: 'Playstation Direct US',
+    slug: 'playstation-direct-us',
     url: 'https://direct.playstation.com/en-us/hardware/ps5',
+    logoPath: './assets/img/playstation-logo.png',
+    country: 'US',
+    currency: 'USD',
     usesPagination: false,
     getPageNumberQueryString: () => '',
     checkEmptyPage: async () => false,
@@ -77,23 +94,32 @@ const sites = [
 
       for (const item of items) {
         const outOfStock = await item.$('.out-stock-wrpr')
+        if (outOfStock) {
+          continue
+        }
         const title = await item.$eval('p[class=sony-text-overline]', element => element.innerText)
         const url = await item.$eval('a', element => element.href)
         const price = await item.$eval('span[class^=product-price]', element => element.innerText)
+        const thumbnail = await item.$eval('img', element => element.src)
         ps5Items.push({
           title,
           url,
           price: `$ ${price}`,
-          hasStock: !outOfStock
+          thumbnail
         })
       }
       return ps5Items
     }
   },
   {
-    vendor: 'BestBuy US',
+    vendor: 'BestBuy',
+    slug: 'bestbuy-us',
     url: 'https://www.bestbuy.com/site/searchpage.jsp?intl=nosplash&st=playstation+5',
+    logo: 'bestbuy-logo.png',
     usesPagination: false,
+    logoPath: './assets/img/bestbuy-logo.png',
+    country: 'US',
+    currency: 'USD',
     getPageNumberQueryString: () => '',
     checkEmptyPage: async () => false,
     getAvailableItems: async ({ page }) => {
@@ -102,14 +128,18 @@ const sites = [
 
       for (const item of items) {
         const outOfStock = await item.$('button[data-button-state=SOLD_OUT]')
+        if (outOfStock) {
+          continue
+        }
         const title = await item.$eval('h4[class=sku-header]>a', element => element.innerText)
         const url = await item.$eval('h4[class=sku-header]>a', element => element.href)
         const price = await item.$eval('div[class^=priceView-hero-price]>span', element => element.innerText)
+        const thumbnail = await item.$eval('img', element => element.src)
         ps5Items.push({
           title,
           url,
           price,
-          hasStock: !outOfStock
+          thumbnail
         })
       }
       return ps5Items
@@ -117,8 +147,8 @@ const sites = [
   }
 ]
 
-const findItemsBySite = async ({ browser, site }) => {
-  const page = await browser.newPage()
+const findItemsBySite = async ({ context, site }) => {
+  const page = await context.newPage()
   let pageNumber = 1
   const foundItems = []
   while (true) {
@@ -143,20 +173,27 @@ const findItemsBySite = async ({ browser, site }) => {
     }
   }
   await page.close()
-  return {
-    vendor: site.vendor,
-    foundItems
-  }
+  return foundItems
 }
 
 ;(async () => {
   const browser = await chromium.launch()
-  const promises = sites.map(site => findItemsBySite({ browser, site }))
-  const itemsBySite = await Promise.all(promises)
-  const itemsByVendor = {}
-  for (const result of itemsBySite) {
-    itemsByVendor[result.vendor] = result.foundItems
+  const context = await browser.newContext({ userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36' })
+  const promises = sites.map(site => findItemsBySite({ context, site }))
+  const result = await Promise.all(promises)
+  const report = {
+    stores: result.map((items, idx) => {
+      const { vendor, slug, logoPath, country, currency } = sites[idx]
+      return {
+        vendor,
+        slug,
+        logo: logoPath,
+        country,
+        currency,
+        products: items
+      }
+    })
   }
-  console.log(JSON.stringify(itemsByVendor, null, 4))
+  console.log(JSON.stringify(report, null, 4))
   await browser.close()
 })()
