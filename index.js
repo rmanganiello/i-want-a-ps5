be1125ae1a0979a95142149db713d2476f1713c6
Renzo Manganiello <renzo@fmanganiello.com.ar>
2022-01-02 13:38:06 -0300

    [Scraper] Add YPF Serviclub scraper

--- index.js
+++ index.js
@@ -138,6 +138,32 @@ const sites = [
     }
   },
   {
+    vendor: 'YPF Serviclub',
+    slug: 'ypf-serviclub-ar',
+    url: 'https://www.serviclub.com.ar/tecnologia/32487-playstation-5.html',
+    logoPath: './assets/img/serviclub-logo.jpg',
+    country: 'AR',
+    currency: 'ARS',
+    usesPagination: false,
+    getPageNumberQueryString: () => '',
+    checkEmptyPage: async () => false,
+    getAvailableItems: async ({ page }) => {
+      const outOfStock = await page.$('.product-unavailable')
+      if (outOfStock) {
+        return []
+      }
+      const title = await page.$eval('div[itemprop=name]', element => element.innerText)
+      const price = await page.$eval('span[itemprop=price]', element => element.innerText)
+      const thumbnail = await page.$eval('img[itemprop=image]', element => element.src)
+      return [{
+        title,
+        url: 'https://www.serviclub.com.ar/tecnologia/32487-playstation-5.html',
+        price,
+        thumbnail
+      }]
+    }
+  },
+  {
     vendor: 'BestBuy',
     slug: 'bestbuy-us',
     url: 'https://www.bestbuy.com/site/searchpage.jsp?intl=nosplash&st=playstation+5',
