fa5a80dadadaeb57a82d1c2e62b996f1d3832163
Renzo Manganiello <renzo@fmanganiello.com.ar>
2022-01-02 21:44:44 -0300

    [Scraper] Add Walmart Argentina Scraper

--- index.js
+++ index.js
@@ -164,6 +164,34 @@ const sites = [
     }
   },
   {
+    vendor: 'Walmart Argentina',
+    slug: 'walmart-ar',
+    url: 'https://www.walmart.com.ar/consola-playstation-5-standard/p',
+    logoPath: './assets/img/walmart-logo.png',
+    country: 'AR',
+    currency: 'ARS',
+    usesPagination: false,
+    getPageNumberQueryString: () => '',
+    checkEmptyPage: async () => false,
+    getAvailableItems: async ({ page }) => {
+      const outOfStock = await page.$('div[class=u-center]>h3')
+      if (outOfStock) {
+        return []
+      }
+      const title = await page.$eval('div[class*=productName]', element => element.innerText)
+      const priceItems = await page.$$('.skuBestPrice>span')
+      const [amount, cents] = await Promise.all(priceItems.slice(1).map(item => item.innerText()))
+      const price = `${amount},${cents}`
+      const thumbnail = await page.$eval('#image-main', element => element.src)
+      return [{
+        title,
+        url: 'https://www.walmart.com.ar/consola-playstation-5-standard/p',
+        price: `$ ${price}`,
+        thumbnail
+      }]
+    }
+  },
+  {
     vendor: 'BestBuy',
     slug: 'bestbuy-us',
     url: 'https://www.bestbuy.com/site/searchpage.jsp?intl=nosplash&st=playstation+5',
